96836f26457e6e3831f93b2e85d030c50cb4bc4e
DaniGonGre <dgonzalezgregores@danielcastelao.org>
2023-01-23 09:12:29 +0100

    creada Activity2

--- app/src/main/java/com/example/intent2/MainActivity.kt
+++ app/src/main/java/com/example/intent2/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.intent2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    // definir el requestCode
+    val RESULTADO_UNO = 1
+    val RESULTADO_DOS = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bCamara = findViewById<Button>(R.id.bCamara)
+
+        bCamara.setOnClickListener {
+            // Crea un Intent para iniciar la segunda actividad
+            val intent = Intent(this, MainActivity2::class.java)
+
+            startActivityForResult(intent, RESULTADO_UNO)
+            startActivityForResult(intent, RESULTADO_DOS)
+
+        }
     }
 }
